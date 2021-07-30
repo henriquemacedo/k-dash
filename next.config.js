55b991ebeb845c83e73759ec12ea6020dfab0505
Henrique Macedo <henrique@hey.com>
2021-07-30 20:22:49 +0100

    Improve forecast mocks

--- next.config.js
+++ next.config.js
@@ -4,12 +4,14 @@ module.exports = {
     weatherZones: [
       {
         country: "Ghana",
+        countryCode: "GH",
         city: "Accra",
         lat: "5.603717",
         long: "-0.186964",
       },
       {
         country: "Nigeria",
+        countryCode: "NG",
         city: "Uyo",
         lat: "5.0405866",
         long: "7.9194225",
